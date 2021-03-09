bbb08040fd00e683f927d3be494a171c2c91ba7e
Azhar Islam <azhartislam@gmail.com>
2021-03-09 14:51:05 +0000

    Added ink types

--- JotPad/ContentView.swift
+++ JotPad/ContentView.swift
@@ -26,13 +26,14 @@ struct Home: View {
     @State var isDraw = true
     @State var color: Color = .black
     @State var type: PKInkingTool.InkType = .pencil
+    @State var colorPicker = false
     
     // default medium is Pen
     
     var body: some View {
         NavigationView{
             //Drawing view
-            DrawingView(canvas: $canvas, isDraw: $isDraw)
+            DrawingView(canvas: $canvas, isDraw: $isDraw, type: $type, color: $color)
                 .navigationTitle("Jot")
                 .navigationBarTitleDisplayMode(.inline)
                 .navigationBarItems(leading: Button(action: {
@@ -45,8 +46,7 @@ struct Home: View {
                 }), trailing: HStack(spacing: 15){
                     
                     Button(action: {
-                        
-                        isDraw.toggle()
+                        isDraw = false
                     }) {
                         Image(systemName: "pencil.slash")
                             .font(.title)
@@ -55,18 +55,21 @@ struct Home: View {
                     //Menu for ink type and color
                     Menu {
                         
-                        ColorPicker(selection: $color) {
+                        Button(action: {
+                            colorPicker.toggle()
+                        }) {
                             Label {
                                 Text("Color")
                             } icon: {
                                 Image(systemName: "eyedropper.full")
                             }
                         }
-                        
+
                         Button(action: {
                             
                             // changing type
-                            
+                            isDraw = true
+                            type = .pencil
                         }) {
                             Label {
                                 Text("Pencil")
@@ -75,7 +78,10 @@ struct Home: View {
                             }
                         }
                         
-                        Button(action: {}) {
+                        Button(action: {
+                            isDraw = true
+                            type = .pen
+                        }) {
                             Label {
                                 Text("Pen")
                             } icon: {
@@ -83,7 +89,10 @@ struct Home: View {
                             }
                         }
                         
-                        Button(action: {}) {
+                        Button(action: {
+                            isDraw = true
+                            type = .marker
+                        }) {
                             Label {
                                 Text("Marker")
                             } icon: {
@@ -98,6 +107,10 @@ struct Home: View {
                     }
 
                 })
+                .sheet(isPresented: $colorPicker) {
+                    ColorPicker("Pick Color", selection: $color)
+                        .padding()
+                }
         }
     }
 }
@@ -108,8 +121,12 @@ struct DrawingView: UIViewRepresentable {
     
     @Binding var canvas: PKCanvasView
     @Binding var isDraw: Bool
+    @Binding var type: PKInkingTool.InkType
+    @Binding var color: Color
     
-    let ink = PKInkingTool(.pencil, color: .black)
+    var ink: PKInkingTool{
+        PKInkingTool(type, color: UIColor(color))
+    }
     let eraser = PKEraserTool(.bitmap)
     
     func makeUIView(context: Context) -> PKCanvasView {
