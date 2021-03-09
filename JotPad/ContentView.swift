2fcb010e957a674d824fff31b7b55a06fe42f7ad
Azhar Islam <azhartislam@gmail.com>
2021-03-09 13:04:21 +0000

    First commit

---
+++ JotPad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JotPad
+//
+//  Created by Azhar on 09/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JotPad/JotPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  JotPadApp.swift
+//  JotPad
+//
+//  Created by Azhar on 09/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct JotPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
